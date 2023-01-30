d3c4997184976e09135f3f44ea4fe21eefe26130
unknown <naufaldec07@gmail.com>
2023-01-30 13:59:26 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -6,26 +6,7 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky");
     }
-
-    // scroll-up button show/hide script
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
-    } else {
-      $(".scroll-up-btn").removeClass("show");
-    }
   });
-
-  $(".navbar .menu li a").click(function () {
-    // applying again smooth scroll on menu items click
-    $("html").css("scrollBehavior", "smooth");
-  });
-
-  // toggle menu/navbar script
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
-  });
-
   // typing text animation script
   var typed = new Typed(".typing", {
     strings: ["Programmer", "Manager", "Developer"],
