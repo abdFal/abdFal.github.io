967f06ac004032caf11cdb0e0ef5f939b5f57290
Abdillah Naufal Lesmana <naufaldec07@gmail.com>
2023-02-09 13:37:47 +0700

    Update

--- php/index.php
+++ php/index.php
@@ -1,7 +1,5 @@
 <?php
 
-if ('/' === $_SERVER['REQUEST_URI']) {
-require 'index.html';
-} if (
-false !== strpos($_SERVER['REQUEST_URI'], '/greeting'))
-{ require 'index.html';}
+if ($_SERVER['REQUEST_URI'] === '/') {
+    require 'index.html';
+}
